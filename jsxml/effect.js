87d9569a3f1b37ca1ca4e5bed46584f60950a7e4
y412000 <y412000@hotmail.com>
2018-11-13 22:43:46 +0800

    background

--- jsxml/effect.js
+++ jsxml/effect.js
@@ -1,81 +1,22 @@
 $(document).ready(function(){
-	let bgimageW = 1920;
-	let bgimageH = 1247; 
 	
-	let winWidth = $(window).width();
-	let winHeight = $(window).height();
-		
-	let ratioW = winWidth / bgimageW;
-	let ratioH = winHeight / bgimageH;
-		
-	if( ratioW > ratioH ) {
-		$("body").css("background-size", ratioW + " auto");
-	} else {
-		$("body").css("background-size", "auto " + ratioH);
-	}
+	backgroundImg();
 	
 	/**************當視窗大小改變時**************/
 	$(window).resize(function(){
-		let winWidth = $(window).width();
-		let winHeight = $(window).height();
 		
-		let ratioW = winWidth / bgimageW;
-		let ratioH = winHeight / bgimageH;
-		
-		if( ratioW > ratioH ) {
-			$("body").css("background-size", (ratioW + 0.02) + " auto");
-		} else {
-			$("body").css("background-size", "auto " + (ratioH + 0.02));
-		}
-		
-	//	alert("width ratio: " + ratioW + ", height: " + ratioH + "\n" + (ratioW + 0.1) + " auto");
-		
-		
-		/**************************************************
-		主瀏覽列背景顏色根據視窗大小及畫面位置更改
-		1. 視窗寬度大於768px且畫面向下滾動時為透明深暗
-		2. 視窗寬度大於768px但畫面置頂時為透明淺暗
-		3. 視窗寬度小於768px，無論畫面位於哪處皆為灰色
-		**************************************************/
-		if( $(window).width() > 768) {
-			if( $("#myMainBar").offset().top > 90) {
-				$("#myMainBar").css("background-color", "rgba(0, 0, 0, 0.7)");
-			} else {
-				$("#myMainBar").css("background-color", "rgba(0, 0, 0, 0.2)");
-			}
-		} else {
-			$("#myMainBar").css("background-color", "rgba(72, 72, 72, 1)");
-		}
-		
+		backgroundImg();
+		mainBarBgcolor();		
+		toppingDisplay();
 		
 	});
 	
 	/**************當視窗滾動時**************/
 	$(window).scroll(function(){
-		/*************************************************
-		主瀏覽列背景顏色根據視窗大小及畫面位置更改
-		1. 視窗寬度大於768px且畫面向下滾動時為透明深暗
-		2. 視窗寬度大於768px但畫面置頂時為透明淺暗
-		3. 視窗寬度小於768px，無論畫面位於哪處皆為灰色
-		*************************************************/
-		if( $(window).width() > 768) {
-			if( $("#myMainBar").offset().top > 90) {
-				$("#myMainBar").css("background-color", "rgba(0, 0, 0, 0.7)");
-			} else {
-				$("#myMainBar").css("background-color", "rgba(0, 0, 0, 0.2)");
-			}
-		} else {
-			$("#myMainBar").css("background-color", "rgba(72, 72, 72, 1)");
-		}
 		
-		/*************************************************
-		控制置頂按鈕在畫面下向滑動一段距離後顯示
-		*************************************************/
-		if( $("#myMainBar").offset().top < 500) {
-			$("#topping").hide();
-		} else {
-			$("#topping").show();
-		}
+		mainBarBgcolor();
+		toppingDisplay();
+
 	});
 	
 	/*點選右下角置頂鈕時，安排捲動動畫至頂端*/
@@ -90,9 +31,56 @@ $(document).ready(function(){
 			let target = $(this.hash);
 			$("html, body").animate({scrollTop: target.offset().top - 115}, 700);
 		}
-
 	})
 
+	
+/*************************************************************************************/		
+	
+	/*隨著瀏覽器視窗大小更動背景圖片大小*/
+	function backgroundImg() {
+		let bgimageW = 1920;
+		let bgimageH = 1247; 
+
+		let winWidth = $(window).width();
+		let winHeight = $(window).height();
+
+		let ratioW = winWidth / bgimageW;
+		let ratioH = winHeight / bgimageH;
+
+		if( ratioW > ratioH ) {			//橫狀長方形
+			$("body").css("background-size", ratioW + " auto");
+		} else {						//豎狀長方形
+			$("body").css("background-size", "auto " + ratioH);
+		}
+	}
+	
+	/*控制主導覽列背景顏色*/
+	function mainBarBgcolor() {
+		/**************************************************
+		1. 視窗寬度大於768px且畫面向下滾動時為透明深暗
+		2. 視窗寬度大於768px但畫面置頂時為透明淺暗
+		3. 視窗寬度小於768px，無論畫面位於哪處皆為灰色
+		**************************************************/
+		if( $(window).width() > 768) {
+			if( $("#myMainBar").offset().top > 90) {
+				$("#myMainBar").css("background-color", "rgba(0, 0, 0, 0.7)");
+			} else {
+				$("#myMainBar").css("background-color", "rgba(0, 0, 0, 0.2)");
+			}
+		} else {
+			$("#myMainBar").css("background-color", "rgba(72, 72, 72, 1)");
+		}
+	}
+	
+	/*點選右下角置頂鈕時，安排捲動動畫至頂端*/
+	function toppingDisplay() {
+		if( $("#myMainBar").offset().top < 500) {
+			$("#topping").hide();
+		} else {
+			$("#topping").show();
+		}
+	}
+	
 	
 });
 
