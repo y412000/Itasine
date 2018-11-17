296faa3a6b54ab8133bad7b7554660700a95a09c
y412000 <y412000@hotmail.com>
2018-11-17 15:46:47 +0800

    padding of newsCtn(width<768)

--- jsxml/effectNews.js
+++ jsxml/effectNews.js
@@ -9,13 +9,18 @@ $(document).ready(function() {
 	
 /*************************************************************************************/	
 	function spacingP() {
-		if( $(window).width() < 768 ) {
+		
+		if( $(window).width() < 768 ) { 
+			
+			// 直向長方形
 			if( $(window).width() < $(window).height() ) {
 				$(".newsCtn > h4").css("padding-bottom", "2.5%");
 				$(".newsCtn > p").css("padding", "2% 5%");
+				$(".newsCtn").css("padding", "10% 2% 5% 2%");
 			} else {
 				$(".newsCtn > h4").css("padding-bottom", "0%");
 				$(".newsCtn > p").css("padding", "0% 5%");
+				$(".newsCtn").css("padding", "2% 2% 5% 2%");
 			}
 		}
 	}
